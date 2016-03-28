fb2aef38a5f53c9b5941534f7ecf10fb95be7f5b
Roger Howard <rogerhoward@gmail.com>
2016-03-27 22:05:54 -0700

    Add core.urls

--- aroundlb/aroundlb/urls.py
+++ aroundlb/aroundlb/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+
+urlpatterns += [
+    url(r'', include('core.urls')),
 ]
--- aroundlb/core/views.py
+++ aroundlb/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html', {
+        'modules': settings.CUSTOM_APPS,
+    })

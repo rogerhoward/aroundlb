787275f43adcf0f7501d19c23a430bb34d6eadc3
Roger Howard <rogerhoward@gmail.com>
2016-03-30 12:02:00 -0700

    Cleanup

--- aroundlb/aroundlb/settings_dev.py
+++ aroundlb/aroundlb/settings_dev.py
@@ -6,3 +6,19 @@ from settings import *
 # --------------------------------------------------
 
 KRPANO_PATH = os.path.join(PROJECT_DIR, 'bin', 'krpano')
+
+# --------------------------------------------------
+# Database settings
+# --------------------------------------------------
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aroundlb',
+        'USER': 'root',
+        'PASSWORD': 'r102938x',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'CONN_MAX_AGE': 10,
+    }
+}
